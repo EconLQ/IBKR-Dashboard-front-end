8080bf1e98c543160ccba2105518770882adc77e
EconLQ <zavliybogdan@gmail.com>
2023-11-15 23:20:43 +0200

    Fixed [object Object] view in component HTML. Added new logic for for edit and delete modals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,10 +30,54 @@ export class AppComponent implements OnInit {
       (response: Position[]) => {
         // update positions from response
         this.positions = response;
+
+        for (let position of this.positions) {
+          const date = position.date as any; // parse date object
+
+          // set date to a custom format
+          position.date = `${date['year']}-${date['monthValue']}-${
+            date['dayOfMonth'] < 10
+              ? '0' + date['dayOfMonth']
+              : date['dayOfMonth']
+          }`;
+        }
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(position: Position, mode: String): void {
+    // set the position data
+    const selectedPositon = position;
+    const selectedMode = mode;
+    // get table
+    const table = document.getElementById('main-container');
+    // create button
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addPositionModal');
+    }
+
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#editPositionModal');
+    }
+
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deletePositionModal');
+    }
+
+    table?.appendChild(button);
+    button.click();
+
+    // open the modal
+    const modal = document.getElementById('editPositionModal');
+    const modalInstance = this.modalService.open(modal);
+    // modalInstance.show();
   }
 }
