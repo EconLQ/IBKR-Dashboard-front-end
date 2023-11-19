18048c56c905314dd24ae632624387803b987d64
EconLQ <zavliybogdan@gmail.com>
2023-11-19 20:19:23 +0200

    Moved logic of the position management to separate component

---
+++ src/app/positions-table/position.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Position } from './position';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PositionService {
+  private apiServerUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) {}
+
+  public getPositions(): Observable<Position[]> {
+    return this.http.get<Position[]>(`${this.apiServerUrl}/positions`);
+  }
+  public addPosition(position: Position): Observable<Position> {
+    return this.http.post<Position>(`${this.apiServerUrl}/positions`, position);
+  }
+
+  public updatePosition(
+    contractId: number,
+    position: Position
+  ): Observable<Position> {
+    return this.http.put<Position>(
+      `${this.apiServerUrl}/positions/${contractId}`,
+      position
+    );
+  }
+
+  public deletePosition(contractId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/positions/${contractId}`
+    );
+  }
+
+  public findPosition(contractId: number): Observable<Position> {
+    return this.http.get<Position>(
+      `${this.apiServerUrl}/positions/${contractId}`
+    );
+  }
+}
