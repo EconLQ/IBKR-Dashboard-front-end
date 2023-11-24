851b7c41ce2bab8ee50b94087126119418497614
EconLQ <zavliybogdan@gmail.com>
2023-11-24 15:46:09 +0200

    Implemented logic to show/hide navbar on login page

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { PositionService } from '../../positions-table/position.service';
+import { NavbarService } from './navbar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-  constructor() {}
+export class NavbarComponent implements OnDestroy {
+  showNavbar: boolean = true;
+  private subscription: Subscription;
+
+  constructor(private navbarService: NavbarService) {
+    this.subscription = this.navbarService.showNavbar.subscribe((value) => {
+      this.showNavbar = value;
+    });
+  }
+
+  /**
+   * Beneficial for performance to unusubscribe from the subcription when the component is destroyed
+   */
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
